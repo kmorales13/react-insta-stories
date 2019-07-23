fe7a99fc4e9d31d71d884868256a5d70d35b3e74
Kevin Morales <32549527+kmorales13@users.noreply.github.com>
2019-07-22 19:21:48 -0500

    Update Container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -143,7 +143,7 @@ Container.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   loader: PropTypes.element,
-  header: PropTypes.element,
+  header: PropTypes.func,
   storyContentStyles: PropTypes.object,
   loop: PropTypes.bool
 }
